ebf01497176df9c75f8c13e9aa6fa6e8589601eb
wilsonap <wilson_alessandro@yahoo.com.br>
2026-08-05 11:04:26 -0300

    chore: bump version to 1.1.5
    Update versionCode to 5 and versionName to 1.1.5 for the new release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
     applicationId = "br.com.autocheckia.memory"
     minSdk = 24
     targetSdk = 35
-    versionCode = 4
-    versionName = "1.0.4"
+    versionCode = 5
+    versionName = "1.1.5"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
