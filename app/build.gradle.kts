02fd727b1a9a4f42c51b70f3d2d21ca8e1caa932
wilsonap <wilson_alessandro@yahoo.com.br>
2026-08-05 09:35:27 -0300

    chore: bump version to 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     applicationId = "br.com.autocheckia.memory"
     minSdk = 24
     targetSdk = 35
-    versionCode = 3
-    versionName = "1.0.3"
+    versionCode = 4
+    versionName = "1.0.4"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
