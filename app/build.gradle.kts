cb4097d82fd9c95eb22801aa1517b4a20e371a6b
wilsonap <wilson_alessandro@yahoo.com.br>
2026-08-09 17:39:09 -0300

    chore: bump version to 1.1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     applicationId = "br.com.autocheckia.memory"
     minSdk = 24
     targetSdk = 36
-    versionCode = 8
-    versionName = "1.1.8"
+    versionCode = 9
+    versionName = "1.1.9"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     manifestPlaceholders["admobAppId"] = "ca-app-pub-9508188839425769~9719772999"
